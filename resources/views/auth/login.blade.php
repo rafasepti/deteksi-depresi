89115f5af0d00b5851297ea06cbe18a1538301aa
rafasepti <rafasepti@gmail.com>
2024-05-22 16:26:34 +0700

    update: style login register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,87 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/pasien') }}/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="{{ asset('assets/pasien') }}/assets/img/favicon.png">
+  <title>
+    Material Kit 2 by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- Nucleo Icons -->
+  <link href="{{ asset('assets/pasien') }}/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="{{ asset('assets/pasien') }}/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('assets/pasien') }}/assets/css/material-kit.css?v=3.0.4" rel="stylesheet" />
+  <!-- Nepcha Analytics (nepcha.com) -->
+  <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+</head>
+
+<body class="sign-in-basic">
+  <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');" loading="lazy">
+    <span class="mask bg-gradient-dark opacity-6"></span>
+    <div class="container my-auto">
+      <div class="row">
+        <div class="col-lg-4 col-md-8 col-12 mx-auto">
+          <div class="card z-index-0 fadeIn3 fadeInBottom">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+                
+              </div>
+            </div>
+            <div class="card-body">
+              <form role="form" class="text-start" method="POST" action="{{ route('login') }}" >
+                @csrf
+                <div class="input-group input-group-outline my-3">
+                  <label class="form-label">Email</label>
+                  <input type="email" class="form-control" id="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                  <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+                <div class="input-group input-group-outline mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" class="form-control" id="password" name="password" required autocomplete="current-password">
+                  <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+                <div class="form-check form-switch d-flex align-items-center mb-3">
+                  <input class="form-check-input" type="checkbox" id="rememberMe"  id="remember_me" name="remember_me">
+                  <label class="form-check-label mb-0 ms-3" for="rememberMe">Remember me</label>
+                </div>
+                <div class="text-center">
+                  <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
+                </div>
+                <a href="{{ route('register') }}">
+                    <p class="mt-4 text-sm text-center">
+                        Don't have an account?
+                    </p>
+                </a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="{{ asset('assets/pasien') }}/assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="{{ asset('assets/pasien') }}/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="{{ asset('assets/pasien') }}/assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+  <script src="{{ asset('assets/pasien') }}/assets/js/material-kit.min.js?v=3.0.4" type="text/javascript"></script>
+</body>
+
+</html>
--- resources/views/layouts/pasien/navbar.blade.php
+++ resources/views/layouts/pasien/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg  blur border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
     <div class="container-fluid px-0">
       <a class="navbar-brand font-weight-bolder ms-sm-3" href="https://demos.creative-tim.com/material-kit/index" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
-        Material Kit 2
+        Deteksi Depresi
       </a>
       <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
@@ -367,13 +367,9 @@
             </ul>
           </li>
           <li class="nav-item ms-lg-auto">
-            <a class="nav-link nav-link-icon me-2" href="https://github.com/creativetimofficial/material-kit" target="_blank">
-              <i class="fa fa-github me-1"></i>
-              <p class="d-inline text-sm z-index-1 font-weight-bold" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Star us on Github">Github</p>
+            <a class="nav-link nav-link-icon me-2" href="{{ route('login') }}">
+              <p class="d-inline text-sm z-index-1 font-weight-bold" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Star us on Github">Login</p>
             </a>
-          </li>
-          <li class="nav-item my-auto ms-3 ms-lg-0">
-            <a href="https://www.creative-tim.com/product/material-kit-pro" class="btn btn-sm  bg-gradient-primary  mb-0 me-1 mt-2 mt-md-0">Upgrade to Pro</a>
           </li>
         </ul>
       </div>
