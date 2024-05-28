7a86a68c8268f8ba85e1c822d7240c14b237c8e8
rafasepti <rafasepti@gmail.com>
2024-05-28 19:07:52 +0700

    update: dashoard admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -49,17 +49,6 @@
                         <div class="col-lg-6 col-7">
                             <h6>Jumlah Diagnosa</h6>
                         </div>
-                        <div class="col-lg-6 col-5 my-auto text-end">
-                            <div class="dropdown float-lg-end pe-4">
-                                <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <i class="fa fa-ellipsis-v text-secondary"></i>
-                                </a>
-                                <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
-                                    <li><a class="dropdown-item border-radius-md" href="javascript:;">Print</a></li>
-                                </ul>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="card-body px-0 pb-2">
